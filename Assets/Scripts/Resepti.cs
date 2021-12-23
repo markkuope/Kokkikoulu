f5f9240113ffdcd50bc4a6ddb9a1ce3d47425287
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-23 10:45:10 +0200

    First Script

---
+++ Assets/Scripts/Resepti.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resepti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Terve teille silkohapset");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
