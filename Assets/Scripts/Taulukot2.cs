4c3c7c47b3331bc407e37a376199b9b62301fa87
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-28 14:55:32 +0200

    Gameobject -taulukko

---
+++ Assets/Scripts/Taulukot2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taulukot2 : MonoBehaviour
+{
+    public GameObject[] palikat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(palikat[2]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
