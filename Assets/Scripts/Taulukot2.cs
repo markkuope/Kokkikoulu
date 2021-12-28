3efcc6f22224059f78603ca7c438cb0e3d170d78
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-28 15:13:01 +0200

    Update Taulukot2.cs

--- Assets/Scripts/Taulukot2.cs
+++ Assets/Scripts/Taulukot2.cs
@@ -6,11 +6,17 @@ public class Taulukot2 : MonoBehaviour
 {
     public GameObject[] palikat;
 
+    public Color[] colors;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(palikat[2]);
+        //Destroy(palikat[2]);
+
+        // Annetaan Capsule -komponentille keltainen väri colors -taulukosta
+        palikat[2].GetComponent<Renderer>().material.color = colors[1];
+
     }
 
     // Update is called once per frame
