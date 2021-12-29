ff2d8f35a62ac2609589c6b1a63d1a6c9386ae74
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-29 15:09:54 +0200

    Get Hrealth

--- Assets/Scripts/GetComponentTest.cs
+++ Assets/Scripts/GetComponentTest.cs
@@ -6,6 +6,10 @@ public class GetComponentTest : MonoBehaviour
 {
     Rigidbody rb;
 
+    //luodaan PlayerHealth -tyyppinen muuttuja ja annetaan sille nimeksi health
+    PlayerHealth health;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +17,17 @@ public class GetComponentTest : MonoBehaviour
         // ja muutetaan väriksi punainen
         GetComponent<Renderer>().material.color = Color.red;
 
-
         //rb = GetComponent<Rigidbody>();
-
         //rb.useGravity = false;
         //rb.mass = 10;
+
+        // Haetaan PlayerHealth -scripti ja tallennetaan se health -muuttujaan
+        health = GetComponent<PlayerHealth>();
+
+        // Muutetaan PlayerHealth -scriptin lives arvoa
+        health.lives = 9;
+
+
     }
 
     // Update is called once per frame
