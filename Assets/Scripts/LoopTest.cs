39d737d180a7a2aed4393d8094eb18c602f65b89
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-29 09:01:11 +0200

    While-silmukka

--- Assets/Scripts/LoopTest.cs
+++ Assets/Scripts/LoopTest.cs
@@ -7,12 +7,27 @@ public class LoopTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(" Ennen for -silmukkaa ");
-        for (int i = 1; i < 6; i++)
+        //print(" Ennen for -silmukkaa ");
+        //for (int i = 1; i < 6; i++)
+        //{
+        //    print(" Markku ");
+        //}
+        //print(" Silmukan jälkeen ");
+
+        print(" Ennen while -silmukkaa ");
+        int i = 1;
+        while (i <= 10)
         {
-            print(" Markku ");
+            //print name 10 times
+            //print(" Markku ");
+            print(i);
+            i++;
         }
         print(" Silmukan jälkeen ");
+
+
+
+
     }
 
     // Update is called once per frame
