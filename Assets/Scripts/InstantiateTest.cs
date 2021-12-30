e29bb3b9387415693402a805fe50becbe3b3de56
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-30 09:35:51 +0200

    instantiate

---
+++ Assets/Scripts/InstantiateTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateTest : MonoBehaviour
+{
+
+    public GameObject ball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball, transform.position, Quaternion.identity);
+        //Quaternion.identity means no rotation
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
