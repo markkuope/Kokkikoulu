e84efdf274b5007a872d8a4bfb8362a55ad8909e
markkuope <72963505+markkuope@users.noreply.github.com>
2021-12-30 10:59:14 +0200

    invokerepeating

--- Assets/Scripts/InstantiateTest.cs
+++ Assets/Scripts/InstantiateTest.cs
@@ -14,27 +14,39 @@ public class InstantiateTest : MonoBehaviour
     {
         //Instantiate(ball, transform.position, Quaternion.identity);
         //Quaternion.identity means no rotation
+        //Invoke("RandomBall", 5f);
+        // Funktiota kutsutaan 2 sekunnin p‰‰st‰, sen j‰lkeen 1 sekunnin v‰lein
+        InvokeRepeating("RandomBall", 2f, 1f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetMouseButtonDown(0)) //Left mouse button
-        //{
-        //    Instantiate(ball, transform.position, Quaternion.identity);
-        //}
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //Left mouse button
         {
-            // K‰ytet‰‰n satunnaislukugeneraattoria, jonka laajuus on pallojen lukum‰‰r‰ 
-
-            int randomNumber = Random.Range(0, ball.Length); // 0, 3
-            
-            Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
+            //    Instantiate(ball, transform.position, Quaternion.identity);
+            CancelInvoke("RandomBall");
         }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    // K‰ytet‰‰n satunnaislukugeneraattoria, jonka laajuus on pallojen lukum‰‰r‰ 
 
+        //    int randomNumber = Random.Range(0, ball.Length); // 0, 3
+            
+        //    Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
+        //}
+    } //update p‰‰ttyy t‰h‰n
 
+    void RandomBall()
+    {
 
+        int randomNumber = Random.Range(0, ball.Length); // 0, 3
+
+        Instantiate(ball[randomNumber], transform.position, Quaternion.identity);
 
     }
+
+
+
 }
