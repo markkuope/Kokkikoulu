9a53f6460b8dec08609c3024ac6732a06be59540
markkuope <72963505+markkuope@users.noreply.github.com>
2022-01-01 15:32:19 +0200

    Mouse input

--- Assets/Scripts/InputTest.cs
+++ Assets/Scripts/InputTest.cs
@@ -6,11 +6,13 @@ public class InputTest : MonoBehaviour
 {
     public float speed;
 
+    public GameObject ball;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -39,15 +41,52 @@ public class InputTest : MonoBehaviour
         //    GetComponent<Renderer>().material.color = Color.green;
         //}
 
-         float xInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        //float xInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
-         float yInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        //float yInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-         print(xInput);
+        //print(xInput);
 
-         transform.Translate(xInput , yInput, 0);
+        //transform.Translate(xInput , yInput, 0);
 
         //Input.GetAxisRaw
 
-    } // update p‰‰ttyy t‰h‰n
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    print(" Left Click");
+        //}
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+
+            //0 = left mouse button, 1 = right mouse button, 2 = mouse wheel
+            //print (" Left Click ");
+
+            //print(Input.mousePosition); // z on aina 0,0 koska screen
+            //print(Input.mousePosition.x + " " + Input.mousePosition.y);
+
+            //Talletetaan hiiren Paikka vektorimuuttujaan pos
+            //Vector3 pos = Input.mousePosition;
+
+            //Hiiren z on aina 0,0, siksi muutettava z, jotta saadaan spawnattua pallo pelimaailmaan
+            //pos.z = 10f;
+
+            //Screen koordinaatit ovat aina 2D screenin pinnassa, siksi nyt muutettava koordinaatit 
+            //world -koordinaateiksi, jotta p‰‰st‰‰n 3D pelimaailman sis‰lle
+
+            //pos = Camera.main.ScreenToWorldPoint(pos);
+
+            //Instantiate(ball, pos, Quaternion.identity);
+        //}
+    }// update p‰‰ttyy t‰h‰n
+
+    void OnMouseDown()
+    {
+
+        Destroy(gameObject);
+
+    }
+
+
+
 }
